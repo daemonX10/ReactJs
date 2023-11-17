294f55ece10b0cc475bdcb29a855e1ca666dbd1f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-18 02:02:42 +0530

    Add script tag to index.html and create Damodar component

---
+++ 01_vite/src/Damodar.jsx
@@ -0,0 +1,9 @@
+function Damodar(){
+    return(
+        <>
+            <h1>It's me , You are looking a art , watching a master</h1>
+        </>
+    )
+}
+
+export default Damodar;
--- 01_vite/src/main.jsx
+++ 01_vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Damodar from './Damodar.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Damodar/>
   </React.StrictMode>,
 )
