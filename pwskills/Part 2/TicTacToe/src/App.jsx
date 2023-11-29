64168285cce64e19c0c1e81af18afb3117da875c
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-30 03:17:39 +0530

    Add title and player information to the game interface

--- pwskills/Part 2/TicTacToe/src/App.jsx
+++ pwskills/Part 2/TicTacToe/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   return (
     <>
+    <h1 style={{textAlign : 'center', color:'whitesmoke'}} >Tic Tac Toe</h1>
+    <h2 style={{textAlign : 'left', color:'whitesmoke', paddingLeft:'5vw'}} >Player 1 : X</h2>
     <Grid numberOfCards={9}/>
     </>
   )
