f8d0e9a4d298889adfe8f1dbee4cd9e1efac3698
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-02 00:43:27 +0530

    Add turn functionality to Grid component

--- pwskills/Part 2/TicTacToe/src/components/Grid/Grid.jsx
+++ pwskills/Part 2/TicTacToe/src/components/Grid/Grid.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react"
 import Card from "../Card/Card"
 import './Grid.css'
 import PropTypes from 'prop-types'
 const Grid = ({numberOfCards=9}) => {
+
+  const [turn, setTurn] = useState(true);
+
+  function play (){
+    console.log('Move Played');
+    setTurn(!turn);
+  }
+
   return (
   <>
     <h1 className="turn-highlight">
-      Current Turn  : 
+      Current Turn  : {turn ? 'X' : 'O'}
     </h1>
     
     <div className="grid">
-      {Array(numberOfCards).fill(<Card />).map((el, idx) => {
-        return <Card key={idx} />
+      {Array(numberOfCards).fill().map((el, idx) => {
+        return <Card onPlay={play} key={idx} />
       })}
     </div>
   </>
