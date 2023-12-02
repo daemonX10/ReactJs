131491d0506f93f9eef365597d2a11326848cc05
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-03 03:57:21 +0530

    Refactor Grid component and fix formatting

--- pwskills/Part 2/TicTacToe/src/components/Grid/Grid.jsx
+++ pwskills/Part 2/TicTacToe/src/components/Grid/Grid.jsx
@@ -1,71 +1,68 @@
-  import { useState } from "react"
-  import Card from "../Card/Card"
-  import './Grid.css'
-  import PropTypes from 'prop-types'
-  import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react"
+import Card from "../Card/Card"
+import './Grid.css'
+import PropTypes from 'prop-types'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import  isWinner  from  "../../helper/CheckWinner.js"
 
+const Grid = ({numberOfCards=9}) => {
 
+const [turn, setTurn] = useState(true);
+const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+const [winner, setWinner] = useState(null);
 
-  const Grid = ({numberOfCards=9}) => {
-
-  const [turn, setTurn] = useState(true);
-  const [board, setBoard] = useState(Array(numberOfCards).fill(""));
-  const [winner, setWinner] = useState(null);
-
-  const playerTurn = () => {
-    return winner == null ? (turn ? 'X' : 'O') : winner;
-  }
-
-  function play (index){
-    console.log('Move Played', index);
-    const newBoard = [...board];
-    if (turn == true) {
-      newBoard[index] = 'X';
-    }
-    else {
-      newBoard[index] = 'O';
-    }
-    const win = isWinner(newBoard, turn ? 'X' : 'O');
-    if (win) {
-      setWinner(win);
-      toast.success(`Winner is ${win}`, {
-        position: "top-right",
-        theme: "colored",
-      });
-    }
-    setBoard([...newBoard]);
-    setTurn(!turn);
+const playerTurn = () => {
+  return winner == null ? (turn ? 'X' : 'O') : winner;
 }
 
-  function reset(){
-    setBoard(Array(numberOfCards).fill(""));
-    setWinner(null);
+function play (index){
+  console.log('Move Played', index);
+  const newBoard = [...board];
+  if (turn == true) {
+    newBoard[index] = 'X';
   }
+  else {
+    newBoard[index] = 'O';
+  }
+  const win = isWinner(newBoard, turn ? 'X' : 'O');
+  if (win) {
+    setWinner(win);
+    toast.success(`Winner is ${win}`, {
+      position: "top-right",
+      theme: "colored",
+    });
+  }
+  setBoard([...newBoard]);
+  setTurn(!turn);
+}
 
-  return (
+function reset(){
+  setBoard(Array(numberOfCards).fill(""));
+  setWinner(null);
+}
+
+return (
   <>
-    <h1 className="turn-highlight">
-        Current Turn  :  {playerTurn()}
-    </h1>
-    <ToastContainer/>
-    <button onClick={reset} className="reset"> Next Game </button>
-    
-    { winner &&  (
-      <h1 className="winner-highlight">
-        Winner is {winner}
-      </h1>
-    )}
 
+  <h1 className="turn-highlight">
+      Current Turn  :  {playerTurn()}
+  </h1>
+  <ToastContainer/>
+  <button onClick={reset} className="reset"> Next Game </button>
+  
+  { winner &&  (
+    <h1 className="winner-highlight">
+      Winner is {winner}
+    </h1>
+)}
     <div className="grid">
       {board.map((value, idx) => {
         return <Card onPlay={play} player={value} key={idx} index={idx} gameEnd = {winner ? true : false}  />
       })}
     </div>
   </>
-  )
-  }
+)}
 
   Grid.propTypes = {
   numberOfCards: PropTypes.number
