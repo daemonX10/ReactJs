949db804192cae76046235d30baa6c60d96ba569
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-05 01:13:21 +0530

    Fix password generator UI layout

--- 05_Password_Generator/src/App.jsx
+++ 05_Password_Generator/src/App.jsx
@@ -46,7 +46,9 @@ function App() {
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-rose-500' >
+
     <h1 className='text-white text-center my-3'>Password Generator</h1>
+
     <div className='flex shadow rounded-lg overflow-hidden mb-4'>
       <input type="text" value={password} className='outline-none w-full py-1 px-3 ' placeholder='Password' readOnly ref={passwordRef} />
         <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5'>Copy</button>
@@ -57,17 +59,20 @@ function App() {
         <input type="range" min={6} max={50} value={length} className='cursor-pointer' onChange={(e)=>{ setLength(e.target.value )}} />
         <label>Length :{length}</label>
       </div>
+
       <div className='flex items-center gap-x-1'>
         <input type="checkbox" 
-        onChange={()=>{setNumberAllowed((prev)=>{!prev})}} />
+        onChange={()=>{setNumberAllowed((prev)=>!prev)}} />
         <label >Numbers</label>
       </div>
+
       <div className='flex items-center gap-x-1'>
         <input type="checkbox" defaultChecked= {charAllowed} id='characterInput' onChange={()=>{
           setCharAllowed((prev)=>!prev)
         }} />
         <label htmlFor="characterInput">Characters</label>
       </div>
+
     </div>
     </div>
   )
