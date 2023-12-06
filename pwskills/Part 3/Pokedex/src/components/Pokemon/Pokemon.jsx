96ccd3f35dcfa881c54f385415c32486df6e8c70
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-07 03:21:59 +0530

    Fix formatting in Pokemon component

--- pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
+++ pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
@@ -1,18 +1,18 @@
-import './Pokemon.css'
-import PropTypes from 'prop-types'
+import './Pokemon.css';
+import PropTypes from 'prop-types';
 
 const Pokemon = ({ name, url }) => {
   return (
-        <div className="pokemon-frame">
-          <img src={url} />
-          <h3>{name}</h3>
-        </div>
-  )
-}
+    <div className="pokemon-frame">
+      <img src={url} />
+      <h3>{name}</h3>
+    </div>
+  );
+};
 
-Pokemon.proptype = {
+Pokemon.propTypes = {
   name: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired
-}
+  url: PropTypes.string.isRequired,
+};
 
 export default Pokemon
