ac5f47dd4bde7d85838d808c593b1becd2b9e344
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-07 20:37:07 +0530

    Add Pokemon ID to Pokemon component

--- pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
+++ pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
@@ -1,18 +1,22 @@
 import './Pokemon.css';
 import PropTypes from 'prop-types';
 
-const Pokemon = ({ name, url }) => {
+const Pokemon = ({ name, url,id }) => {
   return (
-    <div className="pokemon-frame">
-      <img src={url} />
-      <h3>{name}</h3>
-    </div>
+      <div className="pokemon-frame">
+        <p className="pokemon-id">{id}
+        </p>
+        <img src={url} />
+        <h3>{name}</h3>
+      </div>
+
   );
 };
 
 Pokemon.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default Pokemon
--- pwskills/Part 3/Pokedex/src/components/PokemonList/PokemonList.jsx
+++ pwskills/Part 3/Pokedex/src/components/PokemonList/PokemonList.jsx
@@ -1,41 +1,61 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './PokemonList.css'
 import axios from 'axios';
 import Pokemon from '../Pokemon/Pokemon';
 
 const PokemonList = () => {
 
-    const POKEDEX_API = 'https://pokeapi.co/api/v2/pokemon';
+    const Default_URL = 'https://pokeapi.co/api/v2/pokemon';
+    const [POKEDEX_URL, SetPOKEDEX_URL] = useState(Default_URL);
     const [pokemonList, setPokemonList] = useState([]);
+    const [nextUrl, setNextUrl] = useState(Default_URL);
+    const [prevUrl, setPrevUrl] = useState(Default_URL);
     
-    const  downloadPokemon = async ()=>{
-        const respose = await axios.get(POKEDEX_API);
+    const downloadPokemon = useCallback(async () => {
+        const respose = await axios.get(POKEDEX_URL ? POKEDEX_URL : Default_URL);
+
+        setNextUrl(respose.data.next);
+        setPrevUrl(respose.data.previous);
+
         const pokemonResults = respose.data.results;
-        const pokemonPromise = pokemonResults.map((pokemon)=>axios.get(pokemon.url));
+        const pokemonPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
         const pokemonListData = await axios.all(pokemonPromise);
 
-        const pokemonFinalList = pokemonListData.map(pokemonData =>{
-            const pokemon = pokemonData.data;
+        const pokemonFinalList = pokemonListData.map(pokemonData => {
+        const pokemon = pokemonData.data;
+
+        const defaultImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
             return {
-                id:pokemon.id,
-                name:pokemon.name,
-                image: pokemon.sprites.other.dream_world.front_default,
-                types : pokemon.types.map((type)=>type.type.name)
+                id: pokemon.id,
+                name: pokemon.name,
+                image: pokemon.sprites.other.dream_world.front_default ? pokemon.sprites.other.dream_world.front_default : defaultImage ,
+                types: pokemon.types.map((type) => type.type.name)
             }
         });
         setPokemonList(pokemonFinalList);
         console.log(pokemonFinalList);
-    }
+    },[POKEDEX_URL])
 
     useEffect(()=>{
         downloadPokemon();
-    },[])
+    },[downloadPokemon])
 
   return (
     <>
-        <h2> Pokemon List </h2>
+        
+        <div className="page-controller">
+            <button onClick={()=>{
+                SetPOKEDEX_URL(prevUrl);
+            }}>Prev</button>
+            <h2> Pokemon List </h2>
+            <button
+            onClick={()=>{
+                SetPOKEDEX_URL(nextUrl);
+            }}>Next</button>
+        </div>
     <div className="pokemon-list-wrapper">
-        {pokemonList.map((pokemon,index) => <Pokemon name={pokemon.name} url={pokemon.image} key={index} /> )}
+        {pokemonList.map((pokemon,index) => <Pokemon name={pokemon.name} url={pokemon.image} id={pokemon.id} key={index} /> )}
     </div>
     </>
   )
