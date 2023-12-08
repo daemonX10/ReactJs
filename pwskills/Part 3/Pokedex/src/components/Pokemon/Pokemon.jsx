abadcb9627991b80e814e4cd2d2dd15b7a053036
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-08 10:01:45 +0530

    Add react-router-dom package and implement routing in App component

--- pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
+++ pwskills/Part 3/Pokedex/src/components/Pokemon/Pokemon.jsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom';
 import './Pokemon.css';
 import PropTypes from 'prop-types';
 
 const Pokemon = ({ name, url,id }) => {
   return (
-      <div className="pokemon-frame">
-        <p className="pokemon-id">{id}
-        </p>
-        <img src={url} />
-        <h3>{name}</h3>
-      </div>
+      <Link to={`/pokemon/${id}`}>
+        <div className="pokemon-frame">
+          <p className="pokemon-id">{id}
+          </p>
+          <img src={url} />
+          <h3>{name}</h3>
+        </div>
+      </Link>
 
   );
 };
---
+++ pwskills/Part 3/Pokedex/src/components/PokemonDetails/PokemonDetails.jsx
@@ -0,0 +1,9 @@
+import  "./PokemonDetails.css";
+
+const PokemonDetails = () => {
+    return (
+        <h1>PokemonDetails</h1>
+    )
+}
+
+export default PokemonDetails
