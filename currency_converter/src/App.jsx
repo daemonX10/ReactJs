96de67814156a3aed95f07bd27c9acf76069dc07
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-11 02:22:26 +0530

    InputBox

--- currency_converter/src/App.jsx
+++ currency_converter/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
 
   return (
     <>
     <h1 className=" text-3xl bg-rose-300">Currency Converter</h1>
+    <InputBox label="From" />
     </>
   )
 }
