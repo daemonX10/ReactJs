ac8df9550228fe54e3389d26a794a235e7362bd6
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-15 19:14:07 +0530

    Update TailwindCSS styles and configuration

--- TailwindCSS/tailwind.config.js
+++ TailwindCSS/tailwind.config.js
@@ -6,8 +6,35 @@ module.exports = {
     './dist/**/*.html',
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      height:{
+        'screen-1/2': '50vh',
+        'screen-1/3': '33.333333vh',
+        'screen-2/3': '66.666667vh',
+        'screen-1/4': '25vh',
+        'screen-3/4': '75vh',
+        'screen-1/5': '20vh',
+        'screen-2/5': '40vh',
+        'screen-3/5': '60vh',
+        'screen-4/5': '80vh',
+        'screen-1/6': '16.666667vh',
+        'screen-1/7': '14.285714vh',
+        'screen-1/8': '12.5vh',
+        'screen-1/9': '11.111111vh',
+        'screen-1/10': '10vh',
+        'screen-1/15': '6.666667vh',
+        'screen-5/6': '83.333333vh',
+      },
+      zIndex: {
+        '1': 1,
+        '60': 60,
+        '70': 70,
+        '80': 80,
+        '90': 90,
+        '100': 100,
+      },
+      }
+    },
   plugins: [],
 }
 
