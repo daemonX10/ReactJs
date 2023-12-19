10556799c3a004fa0b3401fa7d4a17984b7c01b9
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-19 19:11:26 +0530

    Update search functionality and styling

--- pwskills/Part 3/Pokedex/src/components/Search/Search.jsx
+++ pwskills/Part 3/Pokedex/src/components/Search/Search.jsx
@@ -3,15 +3,24 @@ import './Search.css'
 import  PropTypes from 'prop-types'
 
 const Search = ( {updateSearchTerm} ) => {
-  const debounceUpdateSearch = useDebounce((e) => updateSearchTerm(e.target.value), 2000);
+  const debounceUpdateSearch = useDebounce((e) => updateSearchTerm(e.target.value.toLowerCase()) , 2000);
+  
+  const clearSearch = () => {
+    document.getElementById('search-pokemon').value = ''
+    updateSearchTerm('');
+    debounceUpdateSearch();
+  }
+
   return (
-    <>
+    <div className='search'>
     <input 
     id='search-pokemon'
     type="text" 
     placeholder='Pokemon Name or ID '
     onChange={debounceUpdateSearch} /> 
-    </>
+    <button onClick={clearSearch} >Clear</button>
+    </div>
+
   )
 }
 
