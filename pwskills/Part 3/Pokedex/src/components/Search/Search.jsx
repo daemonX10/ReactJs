d1bd2647bbfea9bb73ad17145b2541f96475366f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-19 10:56:25 +0530

    Add useDebounce hook to Search component

--- pwskills/Part 3/Pokedex/src/components/Search/Search.jsx
+++ pwskills/Part 3/Pokedex/src/components/Search/Search.jsx
@@ -1,14 +1,16 @@
+import useDebounce from '../../hooks/useDebounce'
 import './Search.css'
 import  PropTypes from 'prop-types'
 
 const Search = ( {updateSearchTerm} ) => {
+  const debounceUpdateSearch = useDebounce((e) => updateSearchTerm(e.target.value), 5000);
   return (
     <>
     <input 
     id='search-pokemon'
     type="text" 
     placeholder='Pokemon Name '
-    onChange={(e)=>updateSearchTerm(e.target.value)} /> 
+    onChange={debounceUpdateSearch} /> 
     </>
   )
 }
