a05e8e113c54938323ce7e7f3ffae0a969d60548
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-29 01:46:50 +0530

    Add user registration and login functionality

--- pwskills/Redux/stateManagement.js
+++ pwskills/Redux/stateManagement.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
+import { bindActionCreators, combineReducers, createStore } from "redux";
 
-const TodoReducer = (state, action) => {
+const TodoReducer = (state=[], action) => {
     const nextId = state.length + 1;
     if (action.type === 'ADD_TODO') {
         const trimed_todo = action.payload.todoText.trim();
@@ -42,7 +42,80 @@ const TodoReducer = (state, action) => {
     return state;
 }
 
-const store = createStore(TodoReducer,[{id:1,text:'test',isFinished:false}]);
+const userReducer = (state= [],action)=>{
+    if(action.type==='LOGIN'){
+        return {...state,login:true}
+    }else if(action.type==='LOGOUT'){
+        return {...state,login:false}
+    }else if(action.type==='REGISTER'){
+        return {...state,register:true}
+    }else if(action.type==='UNREGISTER'){
+        return {...state,register:false}
+    }else if(action.type==='CHANGE_NAME'){
+        return {...state,name:action.payload.name}
+    }else if(action.type==='CHANGE_PASSWORD'){
+        return {...state,password:action.payload.password}
+    }
+    else if(action.type==='CHANGE_EMAIL'){
+        return {...state,email:action.payload.email}
+    }else if(action.type==='add_user'){
+        return [...state,{id:action.payload.id,name:action.payload.name,email:action.payload.email,password:action.payload.password}]
+    }
+    return state;
+}
 
-console.log(store);
-console.log(store.getState());
+const addTodo=(todoText)=>{
+    return {type:'ADD_TODO',payload:{todoText:todoText}}
+}
+
+const deleteTodo = (id)=>{
+    return{type:'DELETE_TODO',payload:{id:id}}
+}
+const add_user = (user)=>{
+    return {type:'add_user',payload:{id:user.id,name:user.name,email:user.email,password:user.password}}
+}
+
+const preloadedState = {
+    users:[
+        {
+            id:1,
+            name:'test',
+            email:'nothing@dg.com',
+            password:'1234'
+        }
+    ],
+    todos:[
+        {
+            id:1,
+            text:'test',
+            isFinished:false
+        }
+    ]
+}
+
+
+const reducers = combineReducers({users:userReducer,todos:TodoReducer});
+
+const store = createStore(reducers,preloadedState);
+
+store.subscribe(()=>console.log(store.getState()));
+
+
+
+// BIND ACTION CREATORS 
+
+const actions = bindActionCreators({addTodo,deleteTodo,add_user},store.dispatch);
+
+
+// combine reducers == multiple reducers
+
+
+store.dispatch({type:'LOGIN',payload:{}});
+
+
+actions.add_user([{
+    id:2,
+    name:'test',
+    email:'af@gmail.com',
+    password:'1234'
+}]);
