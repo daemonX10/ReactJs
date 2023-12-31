3dce95573c2036da55c27b1b9d22b602cc99830f
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-31 09:49:13 +0530

    Add React context and Vite configuration files

---
+++ 08_mini_context/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <UserContextProvider>
+    <h1>
+      React Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, autem at? Distinctio itaque fugiat, mollitia expedita rem magnam cupiditate ea eaque possimus enim et totam, facere omnis maiores! Nisi, repellendus!
+    </h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
