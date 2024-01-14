90d6f18748994c8a49141b7aea5fb293820748d5
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-14 10:45:08 +0530

    Add Redux store and components for managing todos

---
+++ react-toolkit/src/features/todoSlice.js
@@ -0,0 +1,30 @@
+
+import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from 'nanoid'
+
+
+const initialState = {
+    todos: [{ id: 1, text: "Hello world" }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            const id = action.payload;
+            state.todos = state.todos.filter(todo => todo.id !== id);
+        }
+    }
+})
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
