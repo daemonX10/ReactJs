47ba3836b7638f863ee7517fd452ced6fedb1ac9
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-14 06:37:13 +0530

    Add React Toolkit files and configuration

---
+++ react-toolkit/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: 'class', // or 'media' or 'class' media for auto dark mode according to system and class for manual
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
